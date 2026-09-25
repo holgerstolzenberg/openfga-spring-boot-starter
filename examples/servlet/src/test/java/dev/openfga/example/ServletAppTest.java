f472dbffbf816cb3973d0719c6343e0035d4f0aa
Holger Stolzenberg <h.stolzenberg@ewerk.com>
2026-09-25 13:22:52 +0200

    chore(example): make example clean buildable

--- examples/servlet/src/test/java/dev/openfga/example/ServletAppTest.java
+++ examples/servlet/src/test/java/dev/openfga/example/ServletAppTest.java
@@ -5,7 +5,10 @@ import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.testcontainers.openfga.OpenFGAContainer;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
+@SpringBootTest
 public class ServletAppTest {
 
     public static void main(String[] args) {
@@ -16,13 +19,11 @@ public class ServletAppTest {
 
     @TestConfiguration(proxyBeanMethods = false)
     static class TestConfig {
-
         @Bean
         OpenFGAContainer container(DynamicPropertyRegistry registry) {
-            OpenFGAContainer container = new OpenFGAContainer("openfga/openfga:v1.4.3");
+            OpenFGAContainer container = new OpenFGAContainer("openfga/openfga:v1.21.0");
             registry.add("openfga.api-url", container::getHttpEndpoint);
             return container;
         }
-
     }
 }
